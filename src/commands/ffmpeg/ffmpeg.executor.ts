c0b24946daa245fac71fc9c856c8e0f3bb14e0a2
Inna Pavlyutenkova <i.pavlyutenkova@devspark>
2022-10-08 14:05:33 +0300

    add ffmpeg executor

---
+++ src/commands/ffmpeg/ffmpeg.executor.ts
@@ -0,0 +1,44 @@
+import { CommandExecutor } from "../../core/executor/command.executor";
+import { ICommandExectFfmpeg, IFfmpegInput } from "./ffmpeg.types";
+import { IStreamLogger } from "../../core/handlers/stream-logger.interface";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+import { FileService } from "../../core/files/file.service";
+import { PromptService } from "../../core/prompt/prompt.service";
+import { FfmpegBuilder } from "./ffmpeg.builder";
+import { StreamHandler } from "../../core/handlers/stream.handler";
+
+export class FfmpegExecutor extends CommandExecutor<IFfmpegInput> {
+    private fileService: FileService = new FileService();
+    private promptService: PromptService = new PromptService();
+
+    constructor(logger: IStreamLogger) {
+        super(logger);
+    }
+
+    protected async prompt(): Promise<IFfmpegInput> {
+        const width = await this.promptService.input<number>('Width', 'number');
+        const height = await this.promptService.input<number>('Height', 'number');
+        const path = await this.promptService.input<string>('Path to file', 'input');
+        const name = await this.promptService.input<string>('Name', 'input');
+        return { width, height, path, name };
+    };
+
+    protected build({width, height, path, name}: IFfmpegInput): ICommandExectFfmpeg {
+        const output = this.fileService.getFilePath(path, name, 'mp4');
+        const args = (new FfmpegBuilder)
+            .input(path)
+            .setVideoSize(width, height)
+            .output(output);
+        return { command: 'ffmpeg', args, output };
+    };
+
+    protected spawn({ output, command, args } : ICommandExectFfmpeg): ChildProcessWithoutNullStreams {
+        this.fileService.deleteFileIfExists(output);
+        return spawn(command, args);
+    };
+
+    protected processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void {
+        const handler = new StreamHandler(logger);
+        handler.processOutput(stream);
+    };
+}
